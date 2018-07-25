70a6241a3bf8bdc442a6455e9c9ebdfce83bc2c4
spacegoing <spacegoing@spacegoings-MacBook-Pro.local>
2018-07-25 17:48:10 +1000

    [New] Finished CSV

---
+++ Market_Event_Spiders/utils/gen_csv.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Market_Events']
+mkt_list = ['hkex']
+col_list = [db[k] for k in mkt_list]
+
+news_df_list = []
+for col in col_list:
+  news_list = list(col.find())
+  news_df = pd.DataFrame(news_list)
+  news_df_list.append(news_df)
+
+df_total = pd.concat(news_df_list)
+df_total.rename(
+    columns={
+        'mkt': 'uptick_name',
+    }, inplace=True)
+
+csv_column_list = [
+    'unique_id', 'uptick_name', 'date', 'title', 'url', 'TRIPI (1)',
+    'TRIPI (2)', 'Category (1)', 'Category (2)'
+]
+df_total = df_total.reindex(columns = csv_column_list)
+df_total.to_csv('chli_csv_upload.csv', index=False)
