c18a5a6321e1b600b5f3ed972053fa9f3b948984
spacegoing <spacegoing@spacegoings-MacBook-Pro.local>
2018-08-15 20:59:32 +1000

    [Refract] format middleware; preparing for random user agent

--- Market_Event_Spiders/market_events_spiders/market_events_spiders/settings.py
+++ Market_Event_Spiders/market_events_spiders/market_events_spiders/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'market_events_spiders.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'market_events_spiders (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 
 # Obey robots.txt rules
 # ROBOTSTXT_OBEY = True
