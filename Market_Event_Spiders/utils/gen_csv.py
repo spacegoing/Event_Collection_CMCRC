04bb42bf78d19c743e65e5e82e4e0f5964d7d037
spacegoing <spacegoing@spacegoings-MacBook-Pro.local>
2018-08-21 12:51:14 +1000

    [New] gen csv udpated

--- Market_Event_Spiders/utils/gen_csv.py
+++ Market_Event_Spiders/utils/gen_csv.py
@@ -3,7 +3,9 @@ import pandas as pd
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 db = client['Market_Events']
-mkt_list = ['hkex']
+mkt_list = [
+    'asx', 'hkex', 'johannesburg', 'nyse', 'shanghai', 'shenzhen', 'tsx', 'lse'
+]
 col_list = [db[k] for k in mkt_list]
 
 news_df_list = []
@@ -13,19 +15,19 @@ for col in col_list:
   news_df_list.append(news_df)
 
 df_total = pd.concat(news_df_list)
-df_total.rename(
-    columns={
-        'mkt': 'uptick_name',
-    }, inplace=True)
+df_total.rename(columns={'mkt': 'uptick_name'}, inplace=True)
+df_total['date'] = df_total['date_time'].dt.date
 
 csv_column_list = [
     'unique_id', 'uptick_name', 'date', 'title', 'url', 'TRIPI (1)',
     'TRIPI (2)', 'Category (1)', 'Category (2)'
 ]
 
+
 def remove_pdf(s):
   return s.split('.pdf')[0]
 
-df_total = df_total.reindex(columns = csv_column_list)
+
+df_total = df_total.reindex(columns=csv_column_list)
 df_total['unique_id'] = df_total['unique_id'].apply(remove_pdf)
 df_total.to_csv('chli_csv_upload.csv', index=False)
